b23683ade912306211af7abb4f055d0f54f87992
Confetto <enockagyei1999@gmail.com>
2023-12-15 14:32:08 +0000

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-// console.log(1)
-// console.log(2)
-// console.log(3)
-// console.log(4)
-// console.log(5)
-// console.log(6)
-// console.log(7)
-// console.log(8)
-// console.log(9)
-// console.log(10)
 
-// for (let i = 1; i <= 10; i++){
-//     console.log(i);
-// }
 
 for (let num = 11; num <= 17; num++){
     console.log("I'm in the loop")
